d08c09f3623f763249bfe17331a6ea481b46256e
malazhatim <malaz.hatim@gmail.com>
2024-08-02 03:52:00 +0000

    mando

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''floor module.
+'''
+
+
+def floor(a: float) -> int:
+    '''Computes the floor of a floating-point number.
+    '''
+    return int(a)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''To str module.
+'''
+
+
+def to_str(a: float) -> str:
+    '''Converts a floating-point number to a string.
+    '''
+    return str(a)
